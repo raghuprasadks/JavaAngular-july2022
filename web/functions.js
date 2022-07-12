bedfbcac4f6e11ec48cc446ecc7ac1cb3d421e32
raghu prasad ks <prasadraghuks@gmail.com>
2022-07-12 13:30:24 +0530

    updated

--- web/functions.js
+++ web/functions.js
@@ -1,19 +1,14 @@
 console.log("functions")
-
 function add(n1,n2){
     result = n1+n2
     console.log(result)    
 }
-
 add(10,20)
-
 function muliply(n1,n2){
     return n1*n2
 }
-
 result = muliply(10,20)
 console.log("multiply ",result)
-
 subtract =function (n1,n2){
     return n1-n2
 }
