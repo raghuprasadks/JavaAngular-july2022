3e1758bcaab89f2dfe6a39eb5775826fc7b6b6c6
raghu prasad ks <prasadraghuks@gmail.com>
2022-07-18 13:37:25 +0530

    updated

--- angular/angularbasics/src/app/components/databindingdemo/databindingdemo.component.ts
+++ angular/angularbasics/src/app/components/databindingdemo/databindingdemo.component.ts
@@ -9,9 +9,15 @@ export class DatabindingdemoComponent implements OnInit {
   principal:number=10000
   roi:number=4
   time:number=1
+  si:number=0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calculateSI():void{
+    console.log('calculateSI')
+    this.si=(this.principal*this.roi*this.time)/100
   }
 
 }
