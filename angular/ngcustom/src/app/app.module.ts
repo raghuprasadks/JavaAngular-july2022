f27af51234a9426488cff5cc5f753e40bbfd2a42
raghu prasad ks <prasadraghuks@gmail.com>
2022-07-27 15:13:43 +0530

    updated

--- angular/ngcustom/src/app/app.module.ts
+++ angular/ngcustom/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
 import { CustompipeComponent } from './components/custompipe/custompipe.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { CustomdirectiveComponent } from './components/customdirective/customdirective.component';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExponentialStrengthPipe,
-    CustompipeComponent
+    CustompipeComponent,
+    HighlightDirective,
+    CustomdirectiveComponent,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
