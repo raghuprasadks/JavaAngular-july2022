d3123bc2ca6380e0233031a13af3e3c0f1f9331f
raghu prasad ks <prasadraghuks@gmail.com>
2022-08-01 11:01:22 +0530

    updated

---
+++ springboot/jpacrudmysql/src/main/java/com/operative/jpacrudmysql/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.operative.jpacrudmysql.repository;
+
+import com.operative.jpacrudmysql.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
+    List<Tutorial> findByTitleContaining(String title);
+
+    List<Tutorial> findByPublished(boolean b);
+}
